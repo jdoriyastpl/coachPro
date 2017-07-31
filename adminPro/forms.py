a9c4af4abdd46273cb21fbd08542a386ce00fbb9
jdoriya@sarvika.com <jdoriya@sarvika.com>
2017-07-31 11:33:51 +0530

    minor updates

--- adminPro/forms.py
+++ adminPro/forms.py
@@ -5,8 +5,8 @@ from django import forms
 
 class UserForm(forms.ModelForm):
     GENDER = (
-    ('Male', 'male'),
-    ('Female', 'female'),
+    ('Male', 'Male'),
+    ('Female', 'Female'),
 
     )
     password = forms.CharField(widget = forms.PasswordInput())
