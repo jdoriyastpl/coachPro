044648b5a589f861ec9f31a091f896aef51c1896
Maksudul Haque <saad.mk11@hotmail.com>
2017-08-05 20:47:43 +0600

    static & media url for devlopment

--- coachPro/urls.py
+++ coachPro/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url,include
 from django.contrib import admin
 from django.conf.urls.static import static
@@ -26,3 +27,7 @@ urlpatterns = [
     url(r'adminPro/course/',include('courses.urls',namespace='course')),
     url(r'adminPro/',include('django.contrib.auth.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
