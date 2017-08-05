2e68f7039ad123b30548423ec3935e831409192c
Maksudul Haque <saad.mk11@hotmail.com>
2017-08-05 20:53:12 +0600

    settings for Static & Media URL

--- coachPro/settings.py
+++ coachPro/settings.py
@@ -14,8 +14,6 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-MEDIA_DIR = os.path.join(BASE_DIR,'media')
-PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 
 # Quick-start development settings - unsuitable for production
@@ -124,10 +122,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_URL = '/static/'
-# STATICFIELS_DIRS = [STATIC_DIR,]
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-#MEDIA
-MEDIA_ROOT = MEDIA_DIR
-MEDIA_URL ='/media/img/'
-# STATIC_ROOT = os.path.join(BASE_DIR,'static')
-DEBUG = True
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static-root")
+
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media-root")
