87e38f36b402dad6bdda987519a7a6f3082b1715
jdoriya@sarvika.com <jdoriya@sarvika.com>
2017-08-08 11:48:11 +0530

    fixed student enrollment.

--- students/forms.py
+++ students/forms.py
@@ -7,4 +7,4 @@ from courses.models import Courses
 class StudentForm(forms.ModelForm):
     class Meta:
         model = Students
-        fields = {'name','course_name','subject','remark'}
+        fields = {'name','course_name','subject','remark','age','father_name','address1','address2','city','state','adhaar_number'}
