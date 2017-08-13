895f1ef3dbd914271a32fb395a41695746e7cca0
Maksudul Haque <saad.mk11@hotmail.com>
2017-08-13 13:52:26 +0600

    model relationship bug fix

--- students/models.py
+++ students/models.py
@@ -39,7 +39,7 @@ class Students(models.Model):
 
 class StudentPaymentDetail(models.Model):
     # user = models.ForeignKey(User,on_delete=models.CASCADE)
-    student = models.OneToOneField(Students,related_name='student_name')
+    student = models.ForeignKey(Students,related_name='student_name')
     course_name = models.ForeignKey(Courses,related_name='course_name')
     paid_amount = models.DecimalField(max_digits=20,decimal_places=2,null=False)
     payment_date = models.DateTimeField(default=timezone.now)
